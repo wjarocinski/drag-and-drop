3cb68c5e7023a05cc628439345fa005cef4ffb51
wjarocinski <wojciech.jarocinski@atos.net>
2019-05-15 15:22:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+import Dndtest from '../src/DndTest/index';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
+`;
+
+const Container = styled.div`
+`;
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        checked: false,
+    }
+  }
+
+  render() {
+    return (
+        <AppWrapper>
+            <Container>
+                <Dndtest />
+            </Container>
+        </AppWrapper>
+    );
+  }
 }
 
 export default App;
